c6cb15705e77775b26cd982985efa12e1602dc6d
Carlos Gabriel Nascimento de Paiva <152037292+gabrielpaiva-projects@users.noreply.github.com>
2025-08-21 22:05:38 -0300

    show

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -13,13 +13,14 @@ class NetworkInfoImpl implements NetworkInfo {
   @override
   Future<bool> get isConnected async {
     final result = await _connectivity.checkConnectivity();
-    return !result.contains(ConnectivityResult.none);
+    result == ConnectivityResult.none;
+    return result != ConnectivityResult.none;
   }
 
   @override
   Stream<bool> get onConnectivityChanged {
     return _connectivity.onConnectivityChanged.map(
-      (results) => !results.contains(ConnectivityResult.none),
+      (results) => results == ConnectivityResult.none,
     );
   }
 }
